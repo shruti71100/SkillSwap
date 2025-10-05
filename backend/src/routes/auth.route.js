000713b72f4a7c80d3346d00cd1114ccca3557cf
shruti71100 <shrutikumari71100@gmail.com>
2025-10-06 01:36:35 +0530

    backend mostly done of the login signup logout onboarding part

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express"
 import {login,signup,logout} from "../controllers/auth.controllers.js"
+import {protectRoute} from "../middleware/auth.middleware.js"
+import {onboard} from "../controllers/auth.controllers.js"
 
 const router = express.Router();
 
@@ -7,6 +9,8 @@ router.post("/signup", signup)
 
 router.post("/login", login)
 router.post("/logout", logout)
+
+router.post("/onboarding",protectRoute,onboard)
 
 /*GET = retrieve data (read-only).
 POST = send data (create or update something).*/
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,12 @@ import express from "express"
 import "dotenv/config"
 import {connectDB} from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
+import cookieParser from "cookie-parser"
 const app = express();
 const PORT= process.env.PORT
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
