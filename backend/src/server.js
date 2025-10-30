4b42a7431f13841c31de17ba453ddb168cd81b67
shruti71100 <shrutikumari71100@gmail.com>
2025-10-30 23:18:21 +0530

    signup made

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,28 @@
-import express from "express"
-import "dotenv/config"
-import {connectDB} from "./lib/db.js"
-import authRoutes from "./routes/auth.route.js"
-import userRoutes from "./routes/user.route.js"
-import chatRoutes from "./routes/chat.route.js"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import express from "express";
+import "dotenv/config";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-
- 
 const app = express();
-const PORT= process.env.PORT
+const PORT = process.env.PORT;
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users",userRoutes);
-app.use("/api/chat",chatRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    connectDB();
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
